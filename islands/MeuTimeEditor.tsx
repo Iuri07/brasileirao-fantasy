41b88f213ce6cb9aa271f6330513d8e8db87aba8
Iuri07 <iuricostermani@hotmail.com>
2026-05-18 21:13:07 -0300

    "Sua escalacao" → "Escalacao" no MeuTimeEditor (cortava no mobile)

--- islands/MeuTimeEditor.tsx
+++ islands/MeuTimeEditor.tsx
@@ -346,7 +346,7 @@ export default function MeuTimeEditor(
       data-selecionado={selecionado ?? undefined}
     >
       <div class="bf-meu-time__bar">
-        <span class="bf-meu-time__titulo">Sua escalacao</span>
+        <span class="bf-meu-time__titulo">Escalacao</span>
         {!aoVivo && fechamentoTexto && (
           <span class="bf-meu-time__market">
             <span class="bf-meu-time__market-dot" aria-hidden="true"></span>
